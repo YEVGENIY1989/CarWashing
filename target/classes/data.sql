ca781d875e365c73db1a11059a5fed063c2ac49f
Jenia <c>
2021-04-23 21:13:24 +0300

    client

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -3,3 +3,19 @@ values ('YevgeniyAdmin' , 'qwerty123');
 
 insert into admins (login, password)
 values ('VasiliyAdmin' , '123qwerty');
+
+
+
+
+insert into service_list(service)
+values ('Мойка');
+
+insert into service_list(service)
+values ('Химчистка');
+
+insert into service_list(service)
+values ('Обработка кузова');
+
+insert into service_list(service)
+values ('Полировка');
+
